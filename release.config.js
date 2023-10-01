d9610b53ee5bfd78fe0ec3b9ade7d8962d69d3aa
Brian Nguyen <vanpho02@gmail.com>
2023-10-01 16:30:20 +0700

    fix: format release file
    Signed-off-by: Brian Nguyen <vanpho02@gmail.com>

--- release.config.js
+++ release.config.js
@@ -1,12 +1,12 @@
 module.exports = {
     branches: [
-        'master'
+        'master',
     ],
     plugins: [
         '@semantic-release/commit-analyzer',
         '@semantic-release/changelog',
         '@semantic-release/npm',
         '@semantic-release/git',
-        '@semantic-release/github'
-    ]
-}
+        '@semantic-release/github',
+    ],
+};
