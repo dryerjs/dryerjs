e576b75aa5ff9daf21ac630ac4fa449b71b4aa62
Brian Nguyen <vanpho02@gmail.com>
2023-10-15 10:15:37 +0700

    optimization: inspect getProperties
    Signed-off-by: Brian Nguyen <vanpho02@gmail.com>

--- src/inspect.ts
+++ src/inspect.ts
@@ -4,11 +4,14 @@ import { Property } from './property';
 import { ApiType, SchemaOptions } from './shared';
 
 const INSPECTED = Symbol('inspected');
+const CACHED_PROPERTIES = Symbol('cached_properties');
 
 export function inspect(modelDefinition: any) {
     if (modelDefinition[INSPECTED]) return modelDefinition[INSPECTED];
     const result = {
+        [CACHED_PROPERTIES]: {},
         getProperties(metaKey: MetaKey = MetaKey.DesignType) {
+            if (this[CACHED_PROPERTIES][metaKey]) return this[CACHED_PROPERTIES][metaKey];
             const result: Property[] = [];
             for (const propertyName in Metadata.getPropertiesByModel(modelDefinition, metaKey)) {
                 const designType = Reflect.getMetadata(
@@ -25,6 +28,7 @@ export function inspect(modelDefinition: any) {
                 const property = new Property(modelDefinition, propertyName, designType, paramTypes);
                 result.push(property);
             }
+            this[CACHED_PROPERTIES][metaKey] = result;
             return result;
         },
         getEmbeddedProperties(): Property[] {
