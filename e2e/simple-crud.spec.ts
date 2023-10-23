375df91115809eda13eb5d334eaa73a7c0adb028
Brian Nguyen <vanpho02@gmail.com>
2023-10-23 14:41:53 +0700

    fix: returns id on get one and e2e
    Signed-off-by: Brian Nguyen <vanpho02@gmail.com>

--- e2e/simple-crud.spec.ts
+++ e2e/simple-crud.spec.ts
@@ -12,11 +12,63 @@ describe('Simple CRUD works', () => {
     await server.start();
   });
 
+  it('Create tags', async () => {
+    const names = ['70s', '80s', '90s'];
+    for (const name of names) {
+      await server.makeSuccessRequest({
+        query: `
+          mutation CreateTag($input: TagInput!) {
+            createTag(input: $input) {
+              id
+              name
+            }
+          }
+        `,
+        variables: {
+          input: {
+            name,
+          },
+        },
+      });
+    }
+  });
+
+  let allTags: Tag[] = [];
+
   it('Get all tags', async () => {
-    const { allTags } = await server.makeSuccessRequest({
-      query: '{ allTags { name } }',
+    const response = await server.makeSuccessRequest({
+      query: `
+      {
+        allTags {
+          id
+          name
+        }
+      }
+      `,
     });
-    expect(allTags).toEqual([]);
+    allTags = response.allTags;
+    expect(allTags).toEqual([
+      { id: expect.any(String), name: '70s' },
+      { id: expect.any(String), name: '80s' },
+      { id: expect.any(String), name: '90s' },
+    ]);
+  });
+
+  it('Get one tag', async () => {
+    const response = await server.makeSuccessRequest({
+      query: `
+      query GetTag($id: ID!) {
+        tag(id: $id) {
+          id
+          name
+        }
+      }
+      `,
+      variables: {
+        id: allTags[0].id,
+      },
+    });
+    expect(response.tag.name).toEqual(allTags[0].name);
   });
 
   afterAll(async () => {
