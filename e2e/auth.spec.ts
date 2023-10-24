5897b95728cdc579929becaf746af6b7803edd08
Truong <truongnd0001@gmail.com>
2023-10-24 23:48:05 +0700

    test: e2e sign up, whoAmi, invalid email

--- e2e/auth.spec.ts
+++ e2e/auth.spec.ts
@@ -1,20 +1,23 @@
 import { TestServer } from './test-server';
 import { User } from '../src/models';
+import { AuthResolver } from '../src/resolvers';
+import * as graphql from 'graphql';
 
 const server = TestServer.init({
   definitions: [User],
+  providers: [AuthResolver],
 });
 
-describe('SignUp Works', () => {
+describe('signUp Works', () => {
   beforeAll(async () => {
     await server.start();
   });
 
-  it('should create a user and then query whoAmI', async () => {
-    const createUserResponse = await server.makeSuccessRequest({
+  test('Create a user', async () => {
+    const signUpResponse = await server.makeSuccessRequest({
       query: `
-      mutation CreateUser($input: CreateUserInput!) {
-        createUser(input: $input) {
+      mutation SignUp($input: CreateUserInput!) {
+        signUp(input: $input) {
           email
           name
           password
@@ -30,33 +33,109 @@ describe('SignUp Works', () => {
       },
     });
 
-    expect(createUserResponse.createUser).toEqual({
+    expect(signUpResponse.signUp).toEqual({
       email: 'dryerjs_123@dryerjs.io',
       name: 'dryerjs_123',
       password: `dryerjs_123`
     });
 
-    // const userId = createUserResponse.createUser.id;
+  });
 
-    // const whoAmIResponse = await server.makeSuccessRequest({
-    //   query: `
-    //   query WhoAmI($userId: String!) {
-    //     whoAmI(userId: $userId) {
-    //       email
-    //       name
-    //       password
-    //     }
-    //   }
-    //   `,
-    //   variables: {
-    //     "input": {
-    //       "userId": userId
-    //     }
-    //   },
-    // });
+  afterAll(async () => {
+    await server.stop();
+  });
+});
 
-    // expect(whoAmIResponse.whoAmI).toBeTruthy();
+describe('whoAmI works', () => {
+  let userId;
 
+  beforeAll(async () => {
+    await server.start();
+    await server.makeSuccessRequest({
+      query: `
+      mutation SignUp($input: CreateUserInput!) {
+        signUp(input: $input) {
+          email
+          name
+          password
+        }
+      }
+      `,
+      variables: {
+        "input": {
+          "email": "dryerjs_123@dryerjs.io",
+          "name": "dryerjs_123",
+          "password": "dryerjs_123"
+        }
+      },
+    });
+
+    const getAllUsersResponse = await server.makeSuccessRequest({
+      query: `
+        query AllUsers {
+          allUsers {
+            email
+            id
+            name
+            password
+          }
+        }
+      `,
+      variables: {
+      },
+    });
+    userId = getAllUsersResponse.allUsers[0].id;
+  });
+
+  test('query whoAmI by userId', async () => {
+    const whoAmIResponse = await server.makeSuccessRequest({
+      query: `
+        query WhoAmI($userId: String!) {
+          whoAmI(userId: $userId) {
+            email
+            name
+            password
+          }
+        }
+      `,
+      variables: {
+        "userId": userId
+      },
+    });
+
+    expect(whoAmIResponse.whoAmI).toBeTruthy();
+  });
+
+  afterAll(async () => {
+    await server.stop();
+  });
+});
+
+describe('invalid email throw error', () => {
+  beforeAll(async () => {
+    await server.start();
+  });
+
+  test('invalid email throw error', async () => {
+     await server.makeFailRequest({
+      query: `
+      mutation SignUp($input: CreateUserInput!) {
+        signUp(input: $input) {
+          email
+          name
+          password
+        }
+      }
+      `,
+      variables: {
+        "input": {
+          "email": "dryerjs_123@dryerjs",
+          "name": "dryerjs_123",
+          "password": "dryerjs_123"
+        }
+      },
+      errorMessageMustContains: 'Bad Request Exception',
+    });
 
   });
 
