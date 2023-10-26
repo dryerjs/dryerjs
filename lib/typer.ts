4234e7a875314c73ed174c9b0455c22190e3aaa9
Brian Nguyen <vanpho02@gmail.com>
2023-10-26 10:32:19 +0700

    refactor: Typer clean up
    Signed-off-by: Brian Nguyen <vanpho02@gmail.com>

--- lib/typer.ts
+++ lib/typer.ts
@@ -1,86 +1,63 @@
 import { ObjectType, InputType } from '@nestjs/graphql';
+import * as util from './util';
 import { Definition } from './shared';
 import { hasScope } from './property';
 import { MetaKey } from './metadata';
 import { inspect } from './inspect';
 
-function getCreateInputType(definition: Definition) {
-  @InputType(`Create${definition.name}Input`)
-  class AbstractCreateInput {}
-  for (const property of inspect(definition).getProperties()) {
-    if (property.name === 'id') continue;
-    const designType = Reflect.getMetadata('design:type', definition.prototype, property.name);
-    Reflect.defineMetadata('design:type', designType, AbstractCreateInput.prototype, property.name);
-    for (const { fn, options } of inspect(definition).for(property.name).get(MetaKey.Thunk)) {
-      if (hasScope(options, 'create')) {
-        fn(AbstractCreateInput.prototype, property.name);
-      }
-    }
-  }
-  return AbstractCreateInput;
-}
-
-function getUpdateInputType(definition: Definition) {
-  @InputType(`Update${definition.name}Input`)
-  class AbstractUpdateInput {}
-  for (const property of inspect(definition).getProperties()) {
-    const designType = Reflect.getMetadata('design:type', definition.prototype, property.name);
-    Reflect.defineMetadata('design:type', designType, AbstractUpdateInput.prototype, property.name);
-    for (const { fn, options } of inspect(definition).for(property.name).get(MetaKey.Thunk)) {
-      if (hasScope(options, 'update')) {
-        fn(AbstractUpdateInput.prototype, property.name);
-      }
-    }
-  }
-  return AbstractUpdateInput;
-}
-
-function getObjectType(definition: Definition) {
-  @ObjectType(definition.name)
-  class AbstractOutput {}
-  for (const property of inspect(definition).getProperties()) {
-    const designType = Reflect.getMetadata('design:type', definition.prototype, property.name);
-    Reflect.defineMetadata('design:type', designType, AbstractOutput.prototype, property.name);
-    for (const { fn, options } of inspect(definition).for(property.name).get(MetaKey.Thunk)) {
-      if (hasScope(options, 'output')) {
-        fn(AbstractOutput.prototype, property.name);
-      }
-    }
-  }
-  return AbstractOutput;
-}
-
-const builtCreateInput = Symbol('builtCreateInput');
-const builtUpdateInput = Symbol('builtUpdateInput');
-const builtOutput = Symbol('builtOutput');
+const cacheKey = Symbol('cached');
 
 export class Typer {
-  // private static getBaseType(input: {
-  //   definition: Definition;
-  //   name: string;
-  //   scope: 'create' | 'update' | 'output';
-  // }) {
-  //   class Abstract
-  // }
+  private static getBaseType(input: {
+    definition: Definition;
+    name: string;
+    scope: 'create' | 'update' | 'output';
+  }) {
+    const cached = input.definition[cacheKey]?.[input.scope];
+    if (cached) return cached;
+
+    const decoratorFn = input.scope === 'output' ? ObjectType : InputType;
+    @decoratorFn(input.name)
+    class Placeholder {}
+
+    for (const property of inspect(input.definition).getProperties()) {
+      const designType = Reflect.getMetadata('design:type', input.definition.prototype, property.name);
+      Reflect.defineMetadata('design:type', designType, Placeholder.prototype, property.name);
+      for (const { fn, options } of inspect(input.definition).for(property.name).get(MetaKey.Thunk)) {
+        if (hasScope(options, input.scope)) {
+          fn(Placeholder.prototype, property.name);
+        }
+      }
+    }
+    input.definition[cacheKey] = {
+      ...util.defaultTo(input.definition[cacheKey], {}),
+      [input.scope]: Placeholder,
+    };
+
+    return input.definition[cacheKey][input.scope];
+  }
 
   public static getCreateInputType(definition: Definition) {
-    if (definition[builtCreateInput]) return definition[builtCreateInput];
-    const result = getCreateInputType(definition);
-    definition[builtCreateInput] = result;
-    return definition[builtCreateInput];
+    return this.getBaseType({
+      definition,
+      name: `Create${definition.name}Input`,
+      scope: 'create',
+    });
   }
 
   public static getUpdateInputType(definition: Definition) {
-    if (definition[builtUpdateInput]) return definition[builtUpdateInput];
-    const result = getUpdateInputType(definition);
-    definition[builtUpdateInput] = result;
-    return definition[builtUpdateInput];
+    return this.getBaseType({
+      definition,
+      name: `Update${definition.name}Input`,
+      scope: 'update',
+    });
   }
 
   public static getObjectType(definition: Definition) {
-    if (definition[builtOutput]) return definition[builtOutput];
-    const result = getObjectType(definition);
-    definition[builtOutput] = result;
-    return definition[builtOutput];
+    return this.getBaseType({
+      definition,
+      name: definition.name,
+      scope: 'output',
+    });
   }
 }
