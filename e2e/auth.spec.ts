80c0f387ba0cfa437d72d122b1f1cacb571d45e6
Truong <truongnd0001@gmail.com>
2023-10-26 10:38:26 +0700

    fix: test whoAmI

--- e2e/auth.spec.ts
+++ e2e/auth.spec.ts
@@ -8,7 +8,7 @@ const server = TestServer.init({
 });
 
 describe('Auth Resolver works', () => {
-  let userId;
+  let user;
 
   beforeAll(async () => {
     await server.start();
@@ -21,7 +21,7 @@ describe('Auth Resolver works', () => {
         signUp(input: $input) {
           email
           name
-          password
+          id
         }
       }
       `,
@@ -37,62 +37,30 @@ describe('Auth Resolver works', () => {
     expect(signUpResponse.signUp).toEqual({
       email: 'dryerjs_123@dryerjs.io',
       name: 'dryerjs_123',
-      password: `dryerjs_123`,
+      id: expect.any(String),
     });
+    user = signUpResponse.signUp;
   });
 
   it('whoIamI works', async () => {
-    await server.makeSuccessRequest({
-      query: `
-      mutation SignUp($input: CreateUserInput!) {
-        signUp(input: $input) {
-          email
-          name
-          password
-        }
-      }
-      `,
-      variables: {
-        input: {
-          email: 'dryerjs_123@dryerjs.io',
-          name: 'dryerjs_123',
-          password: 'dryerjs_123',
-        },
-      },
-    });
-
-    const getAllUsersResponse = await server.makeSuccessRequest({
-      query: `
-        query AllUsers {
-          allUsers {
-            email
-            id
-            name
-            password
-          }
-        }
-      `,
-      variables: {},
-    });
-
-    userId = getAllUsersResponse.allUsers[0].id;
-
     const whoAmIResponse = await server.makeSuccessRequest({
       query: `
         query WhoAmI($userId: String!) {
           whoAmI(userId: $userId) {
             email
             name
-            password
           }
         }
       `,
       variables: {
-        userId: userId,
+        userId: user.id,
       },
     });
 
-    expect(whoAmIResponse.whoAmI).toBeTruthy();
+    expect(whoAmIResponse.whoAmI).toEqual({
+      email: 'dryerjs_123@dryerjs.io',
+      name: 'dryerjs_123',
+    });
   });
 
   it('invalid email throw error', async () => {
@@ -102,7 +70,6 @@ describe('Auth Resolver works', () => {
         signUp(input: $input) {
           email
           name
-          password
         }
       }
       `,
