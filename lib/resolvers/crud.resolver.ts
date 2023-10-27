1d05f8a018e80146811aa3d8168c7f5ca0cc9de3
Bach Hoang <5968183+bachhm-dev@users.noreply.github.com>
2023-10-28 00:26:58 +0700

    feat: simple paginate api (#120)

--- lib/resolvers/crud.resolver.ts
+++ lib/resolvers/crud.resolver.ts
@@ -102,13 +102,14 @@ export function createResolver(definition: Definition): Provider {
     }
 
     @IfApiAllowed(Query(() => PaginatedOutputType(definition)), 'paginate')
-    async [`paginate${util.plural(definition.name)}`]() {
-      const { docs, totalDocs, totalPages, page } = await this.model.paginate({}, { page: 1, limit: 10 });
+    async [`paginate${util.plural(definition.name)}`](
+      @Args('page', { type: () => graphql.GraphQLInt, defaultValue: 1 }) page: number,
+      @Args('limit', { type: () => graphql.GraphQLInt, defaultValue: 10 }) limit: number,
+    ) {
+      const response = await this.model.paginate({}, { page, limit });
       return plainToInstance(PaginatedOutputType(definition), {
-        docs: docs.map((doc) => appendIdAndTransform(definition, doc)),
-        totalDocs,
-        page,
-        totalPages,
+        ...response,
+        docs: response.docs.map((doc) => appendIdAndTransform(definition, doc)),
       });
     }
   }
