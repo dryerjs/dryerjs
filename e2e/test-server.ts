d589ad2f8a95057fd5137a9c1656d02063cad7c8
Brian Nguyen <vanpho02@gmail.com>
2023-10-28 10:07:05 +0700

    fix: merge error
    Signed-off-by: Brian Nguyen <vanpho02@gmail.com>

--- e2e/test-server.ts
+++ e2e/test-server.ts
@@ -7,6 +7,7 @@ import { Definition, DryerModule } from 'dryerjs';
 import { GraphQLError } from 'graphql';
 import * as request from 'supertest';
 import * as mongoose from 'mongoose';
+import * as mongoosePaginateV2 from '../lib/js/mongoose-paginate-v2';
 import * as util from '../lib/util';
 import { APP_FILTER } from '@nestjs/core';
 
@@ -41,7 +42,7 @@ class TestAppModule {
         MongooseModule.forFeature(
           config.definitions.map((definition) => {
             const schema = SchemaFactory.createForClass(definition);
-            // schema.plugin(mongoosePaginateV2);
+            schema.plugin(mongoosePaginateV2);
             return {
               name: definition.name,
               schema,
