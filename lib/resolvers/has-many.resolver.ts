9cc5fdb547e93feb3a176be24085480399e99f93
Brian Nguyen <vanpho02@gmail.com>
2023-11-10 15:41:16 +0700

    feat: user base service for relations
    Signed-off-by: Brian Nguyen <vanpho02@gmail.com>

--- lib/resolvers/has-many.resolver.ts
+++ lib/resolvers/has-many.resolver.ts
@@ -1,7 +1,5 @@
 import * as DataLoader from 'dataloader';
 import { Resolver, Parent, ResolveField } from '@nestjs/graphql';
-import { Model } from 'mongoose';
-import { InjectModel } from '@nestjs/mongoose';
 import { Provider } from '@nestjs/common';
 
 import { MetaKey, Metadata } from '../metadata';
@@ -9,7 +7,8 @@ import { OutputType } from '../type-functions';
 import { Definition } from '../definition';
 import { HasManyConfig } from '../property';
 import { ContextDecorator } from '../context';
-import { ObjectId } from '../shared';
+import { BaseService, InjectBaseService } from '../base.service';
+import { StringLikeId } from '../shared';
 
 export function createResolverForHasMany(
   definition: Definition,
@@ -21,16 +20,15 @@ export function createResolverForHasMany(
 
   @Resolver(() => OutputType(definition))
   class GeneratedResolverForHasMany<T> {
-    constructor(@InjectModel(relationDefinition.name) public model: Model<any>) {}
+    constructor(@InjectBaseService(relationDefinition) public baseService: BaseService) {}
 
     @ResolveField()
     async [field](@Parent() parent: any, @contextDecorator() ctx: any): Promise<T[]> {
-      ctx;
-      const loader = new DataLoader<ObjectId, any>(async (keys) => {
+      const loader = new DataLoader<StringLikeId, any>(async (keys) => {
         const field = relation.options.to;
-        const items = await this.model.find({ [field]: { $in: keys } });
-        return keys.map((id: ObjectId) => {
-          return items.filter((item) => item[field].toString() === id.toString());
+        const items = await this.baseService.findAll(ctx, { [field]: { $in: keys } }, {});
+        return keys.map((id) => {
+          return items.filter((item) => String(item[field]) === String(id));
         });
       });
       return await loader.load(parent._id);
