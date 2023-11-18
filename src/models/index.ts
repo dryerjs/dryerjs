9a7bf85543a7ac7dcc54e5c53f8777d80dc6ba60
Natrix <76524304+luongtrieuvy202@users.noreply.github.com>
2023-11-18 14:56:32 +0700

    add collection for test skip default hooks

--- src/models/index.ts
+++ src/models/index.ts
@@ -3,3 +3,4 @@ export * from './user';
 export * from './product';
 export * from './customer';
 export * from './computer';
+export * from './order';
---
+++ src/models/order.ts
@@ -0,0 +1,27 @@
+import * as graphql from 'graphql';
+import { Property, Definition, ObjectId, Id, GraphQLObjectId, ReferencesMany } from '../../lib';
+
+@Definition({ allowedApis: '*' })
+export class Item {
+  @Id()
+  id: ObjectId;
+
+  @Property({ type: () => graphql.GraphQLString })
+  name: string;
+}
+
+@Definition({ allowedApis: '*', skipDefaultHookMethods: ['all'] })
+export class Order {
+  @Id()
+  id: ObjectId;
+
+  @Property({ type: () => [GraphQLObjectId], nullable: true, db: { type: [ObjectId], default: [] } })
+  itemIds: ObjectId[];
+
+  @ReferencesMany(() => Item, {
+    from: 'itemIds',
+    allowCreateWithin: true,
+    noPopulation: false,
+  })
+  items: Item[];
+}
