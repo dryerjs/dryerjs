7e3abe2e001c1ed208c0ea4dbad3d8e04f9fdf27
Brian Nguyen <vanpho02@gmail.com>
2023-12-11 15:15:54 +0700

    chore: add x-dryerjs-api-key for dev env (#279)
    Signed-off-by: Brian Nguyen <vanpho02@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
 import { AppModule } from './app.module';
+
+@Injectable()
+export class HeaderGuard implements CanActivate {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const SECRET_API_KEY = process.env.SECRET_API_KEY;
+    if (!SECRET_API_KEY) return true;
+    const req = GqlExecutionContext.create(context).getContext().req;
+    return req.header('x-dryerjs-api-key') === SECRET_API_KEY;
+  }
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalGuards(new HeaderGuard());
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
