1b33c04ffa1e54ffcc89208e7e9d29d45e7ec624
Brian Nguyen <vanpho02@gmail.com>
2023-12-23 13:43:48 +0700

    fix: seems done
    Signed-off-by: Brian Nguyen <vanpho02@gmail.com>

--- e2e/base-model.spec.ts
+++ e2e/base-model.spec.ts
@@ -21,20 +21,20 @@ export class BaseModel {
   updatedAt: Date;
 }
 
-@Definition({
-  allowedApis: '*',
-  timestamps: true,
-  removalConfig: {
-    allowCleanUpRelationsAfterRemoved: true,
-  },
-})
+@Definition({ timestamps: true })
 export class Tag extends BaseModel {
   @Property()
   name: string;
 }
 
+@Definition({ timestamps: true })
+export class Product extends BaseModel {
+  @Property()
+  name: string;
+}
+
 const server = TestServer.init({
-  definitions: [Tag],
+  definitions: [Tag, Product],
 });
 
 describe('BaseModel works', () => {
@@ -62,6 +62,31 @@ describe('BaseModel works', () => {
     });
 
     expect(createTag).toEqual({
+      id: expect.any(String),
+      name: 'test',
+      createdAt: expect.any(String),
+      updatedAt: expect.any(String),
+    });
+
+    const { createProduct } = await server.makeSuccessRequest({
+      query: `
+        mutation CreateProduct($input: CreateProductInput!) {
+          createProduct(input: $input) {
+            id
+            name
+            createdAt
+            updatedAt
+          }
+        }
+      `,
+      variables: {
+        input: {
+          name: 'test',
+        },
+      },
+    });
+
+    expect(createProduct).toEqual({
       id: expect.any(String),
       name: 'test',
       createdAt: expect.any(String),
