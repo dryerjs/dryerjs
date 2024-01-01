fd7f55ca3f2517d529d6ab2f1700495d86ac1ccf
Brian Nguyen <vanpho02@gmail.com>
2024-01-01 15:29:59 +0700

    fix: using nest common errors
    Signed-off-by: Brian Nguyen <vanpho02@gmail.com>

--- src/resolvers/auth.resolver.ts
+++ src/resolvers/auth.resolver.ts
@@ -1,8 +1,7 @@
-import * as graphql from 'graphql';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { Args, Mutation, Resolver, Query } from '@nestjs/graphql';
-import { ValidationPipe } from '@nestjs/common';
+import { NotFoundException, ValidationPipe } from '@nestjs/common';
 import { OutputType, CreateInputType } from 'dryerjs';
 import { User } from '../models';
 import { Ctx } from '../ctx';
@@ -30,7 +29,7 @@ export class AuthResolver {
   async whoAmI(@Ctx() context: { userId: string }) {
     const user = await this.User.findById(context.userId);
     if (user === null) {
-      throw new graphql.GraphQLError('User not found');
+      throw new NotFoundException('User not found');
     }
     return user;
   }
